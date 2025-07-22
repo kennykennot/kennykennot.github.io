2daae6401fe55b377f9c7b3a8ce08d3a68a1a39d
kennykennot <kendelirious@gmail.com>
2025-07-22 11:22:34 +0800

    Create typeWriter.js

---
+++ js/typeWriter.js
@@ -0,0 +1,36 @@
+
+let i = 0;
+let text1 = "(#`O′) ！李绝情！";
+let text2 = "一周年快乐呀宝贝！要一直爱下去，越来越爱的那种哟！"
+let speed = 100;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
